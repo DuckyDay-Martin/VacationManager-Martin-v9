79487e59e3ac27284e21c93e4afe79d0b5ab154b
DuckyDay-Martin <mone1@abv.bg>
2023-04-09 10:18:41 +0300

    Оправяне на бъгове

--- VacationManager-Martin/Controllers/HomeController.cs
+++ VacationManager-Martin/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using VacationManager_Martin.Models;
 namespace VacationManager_Martin.Controllers
 {
     //[Authorize(Roles = Global.RoleConstants.Roles.CEO)]
-    [Authorize(Roles = "CEO")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
